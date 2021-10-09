bbc96bd41bf68c34f1b27ddbaf5c9cba1ba77d83
iCloud.Yin <xdyinay@qq.com>
2021-10-09 11:25:06 +0800

    初始化工程

---
+++ src/main/kotlin/cn/wegrowth/usercenter/UserCenterApplication.kt
@@ -0,0 +1,13 @@
+package cn.wegrowth.usercenter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+
+@SpringBootApplication
+@EnableDiscoveryClient
+class UserCenterApplication
+
+fun main(args: Array<String>) {
+	runApplication<UserCenterApplication>(*args)
+}
